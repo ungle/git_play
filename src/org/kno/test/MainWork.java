24b4accfc537512c388c1111d5480e00891a33fc
liangjing <jingpennyliang@11>
2022-06-21 15:08:51 +0800

    init project

---
+++ src/org/kno/test/MainWork.java
@@ -0,0 +1,12 @@
+package org.kno.test;
+
+/**
+ * @author liangjing
+ *
+ */
+public class MainWork {
+	public static void main(String[] args) {
+		System.out.println("works how it may happy 中文");
+	}
+
+}
