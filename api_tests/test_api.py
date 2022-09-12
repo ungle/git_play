ed31e6972dac5977a3d94653f7ef396c61ff5c4b
liangjing <jingpennyliang@outlook.com>
2022-09-12 16:17:20 +0800

    [+] add api functional tests

---
+++ api_tests/test_api.py
@@ -0,0 +1,7 @@
+import requests as requests
+def test_hello():
+	r = requests.get("http://52.77.244.25:9099/hello")
+	assert r.text == "hello!"
+def test_bye():
+	r = requests.get("http://52.77.244.25:9099/bye")
+	assert r.text == "bye!"
